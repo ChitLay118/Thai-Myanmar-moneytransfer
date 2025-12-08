6b7d7a0e9597adb7fd7472d273f3b953c17a1b21
ChitLay118 <c388925@gmail.com>
2025-12-08 13:04:19 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,25 @@
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD__QvXIdX1iE3SwhiGP00lTDz8PO9qrXo",
+    authDomain: "moviebox-b10da.firebaseapp.com",
+    projectId: "moviebox-b10da",
+    storageBucket: "moviebox-b10da.firebasestorage.app",
+    messagingSenderId: "178535518366",
+    appId: "1:178535518366:web:1160ac704c5194a8c16b8f",
+    measurementId: "G-C4W8MJ6R83"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Export for use in other files
+export { auth, db, storage };
